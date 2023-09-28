e661f5e0b637a24c9ffcf8e9477ad220cd16b88f
KevinJPF <123981519+KevinJPF@users.noreply.github.com>
2023-09-28 13:17:18 +0000

    Initial commit

---
+++ exe01.c
@@ -0,0 +1,26 @@
+#include "fila.h"
+#include "pilha.h"
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void) {
+  char s[256];
+  Fila F = fila(256);
+  Pilha P = pilha(256);
+  printf("\nFrase? ");
+  gets(s);
+  for (int i = 0; s[i]; i++)
+    if (isalpha(toupper(s[i]))) {
+      enfileira(toupper(s[i]), F);
+      empilha(toupper(s[i]), P);
+    }
+  while (!vaziaf(F) && desenfileira(F) == desempilha(P))
+    ;
+  if (vaziaf(F))
+    puts("A frase e palindroma");
+  else
+    puts("A frase nao e palindroma");
+  destroif(&F);
+  destroip(&P);
+  return 0;
+}
